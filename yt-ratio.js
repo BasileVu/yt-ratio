12bf887b88f012eed05156841aba984264d12d9f
Basile Vu <basile.vu@gmail.com>
2017-02-03 22:00:34 +0100

    Ranking have max size -> rankings computed on values save

--- yt-ratio.js
+++ yt-ratio.js
@@ -12,6 +12,9 @@
     'use strict';
 
     const LS_KEY = "us-yt-ratio";
+    const RANKING_LOWEST_VIEW_COUNT = 100; // the smallest view count possible for a video to be the first ranking
+    const RANKING_STEP = 10; // the step (as a factor) between each ranking
+    const RANKING_MAX_VIDEOS = 10; // the step (as a factor) between each ranking
 
     /**
     * Retrieves values related to current video and stores them in an object, that contains:
@@ -35,10 +38,10 @@
         let id = document.location.href.match(".*?v=([^&]*)")[1];
         let title = document.querySelector("#eow-title").title;
 
-        let viewCount = parseInt(document.querySelector(".watch-view-count").textContent.split(/[\s,]+/).slice(0, -1).join(""));
+        let viewCount = parseInt(document.querySelector(".watch-view-count").textContent.split(/[\s,.]+/).slice(0, -1).join(""));
 
-        let likes = parseInt(values[0].textContent.replace(/[\s,]+/g, ""));
-        let dislikes = parseInt(values[1].textContent.replace(/[\s,]+/g, ""));
+        let likes = parseInt(values[0].textContent.replace(/[\s,.]+/g, ""));
+        let dislikes = parseInt(values[1].textContent.replace(/[\s,.]+/g, ""));
 
         return {
             "id": id,
@@ -81,18 +84,15 @@
     }
 
     /**
-    * Saves the values of the current video in the local storage. If the number of records stored is bigger than a given maximum value, removes oldest record until
-    * the number of records respects this maximum value.
+    * Saves the values of the current video in the local storage and computes the ranking of the videos.
+    *
+    * When a ranking has too many videos, removes videos that are low-ranked in this ranking.
     *
     * @param {Object} values - The values retrieved from the current video.
-    * @param {Number} maxNumberOfRecords - The maximum number of records to store.
+    * @param {Number} maxPerRanking - The maximum number of videos per ranking.
     * @return {Object} The saved records.
     */
-    function saveValues(values, maxNumberOfRecords) {
-        if (maxNumberOfRecords < 0) {
-            throw "Max number of records can't be negative.";
-        }
-
+    function saveValuesAndComputeRankings(values, maxPerRanking) {
         let records = JSON.parse(localStorage.getItem(LS_KEY));
         if (records === null) {
             records = {};
@@ -101,18 +101,27 @@
         delete records[values.id];
         records[values.id] = values;
 
-        // FIXME use rankings later and remove low-ranked ones
-        // delete old records until their number is small enough
-        while (Object.keys(records).length - maxNumberOfRecords > 0) {
-            delete records[Object.keys(records)[0]];
+        let rankings = computeRankings(records, RANKING_LOWEST_VIEW_COUNT, RANKING_STEP);
+
+        for (let i = 0; i < rankings.length; ++i) {
+
+            // if the ranking is too big, truncate it and delete all low-ranked videos records
+            let ranking = rankings[i];
+            if (ranking.length > maxPerRanking) {
+                let toDelete = ranking[i].slice(maxPerRanking);
+                for (let j = 0; j < toDelete.length; ++j) {
+                    delete records[ranking[j].id];
+                }
+                rankings[i] = ranking.slice(0, maxPerRanking);
+            }
         }
 
         localStorage.setItem(LS_KEY, JSON.stringify(records));
 
-        return records;
+        return rankings;
     }
 
-    function buildRankings(records, from, step) {
+    function computeRankings(records, from, step) {
         if (from <= 0) {
             throw "Smallest view count in rankings can't be <= 0.";
         }
@@ -149,6 +158,13 @@
                 }
             }
         }
+
+        for (let i = 0; i < rankings.length; ++i) {
+            rankings[i].sort(function (a, b) {
+                return b.ratio - a.ratio;
+            });
+        }
+
         return rankings;
     }
 
@@ -162,18 +178,16 @@
         // the video is worth registering (has likes and dislikes)
         if (values !== null) {
             displayRatio(values.ratio);
-            let records = saveValues(values, 5);
-
-            // FIXME remove later
-            console.log("Videos records:");
-            for (let key in records) {
-                if (records.hasOwnProperty(key)) {
-                    console.log(records[key]);
-                }
-            }
+            let rankings = saveValuesAndComputeRankings(values, RANKING_MAX_VIDEOS);
             console.log("Rankings:");
-            let rankings = buildRankings(records, 100, 10);
-            console.log(rankings);
+
+            let lower = RANKING_LOWEST_VIEW_COUNT;
+            let upper = RANKING_LOWEST_VIEW_COUNT;
+            for (let i = 0; i < rankings.length; ++i) {
+                lower = upper;
+                upper = lower * RANKING_STEP;
+                console.log("from", lower, "to", upper, ":", rankings[i]);
+            }
         }
     }
 
